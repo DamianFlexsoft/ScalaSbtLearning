670a6aa7b0a18ca830be02600c8abae437662049
Damian Sobański <damian.flexsoft@hotmail.com>
2016-02-10 21:19:35 +0100

    initial commit

---
+++ hw.scala
@@ -0,0 +1,10 @@
+object Hi {
+    def main(args: Array[String]) = println("Hi!")
+}
+
+lazy val root = (project in file(".")).
+    settings(
+        name := "hello",
+        version := "1.0",
+        scalaVersion := "2.11.5"
+    )
