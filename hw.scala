3e6df4ab0877a501fccf5d0cc2b4883fa4627a14
Damian Sobański <damian.flexsoft@hotmail.com>
2016-02-10 21:33:26 +0100

    hw scala changes

--- hw.scala
+++ hw.scala
@@ -1,10 +1,3 @@
 object Hi {
     def main(args: Array[String]) = println("Hi!")
 }
-
-lazy val root = (project in file(".")).
-    settings(
-        name := "hello",
-        version := "1.0",
-        scalaVersion := "2.11.5"
-    )
